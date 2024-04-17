4b8400f541dd3e0810ef85bf7923155135ecb21c
Hoda Sedighi <sedighi.iut@gmail.com>
2024-04-17 10:32:59 -0400

    call and test the solution

--- SmallestStringStartingFromLeaf/Program.cs
+++ SmallestStringStartingFromLeaf/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿internal class Program  {
+   private static void Main(string[] args){
+        //[0,1,2,3,4,3,4]
+        TreeNode root = new TreeNode(0);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.left = new TreeNode(3);
+        root.right.right = new TreeNode(4);
+        Solution sol = new Solution();
+        System.Console.WriteLine(sol.SmallestFromLeaf(root));
+    }
+}
